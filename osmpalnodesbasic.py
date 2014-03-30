d6e22cb1d12de5989c1f99eaf7ae2a8d61f7adde
Peter Chin <bpchin@facebook.com>
2014-03-29 18:22:52 -0700

    Update osmpalnodesbasic.py

--- osmpalnodesbasic.py
+++ osmpalnodesbasic.py
@@ -1,28 +1,73 @@
 #!/usr/bin/python
-# Tool to read OpenStreetMap .osm files and find nearest and farthest nodes. 
 import sys
 import os
-PYOSM_DIR = os.path.join(os.path.dirname(__file__), ' INSERT THE PATH OF YOUR PYOSM FILE, e.g. /home/peter/osmpal/lib/')
+PYOSM_DIR = os.path.join(os.path.dirname(__file__), 'ENTER THE DIRECTORY LOCATION OF THE PYOSM FILE HERE, e.g. /home/peter/osmpal/lib/')
 sys.path.append(PYOSM_DIR)
 import pyosm
-osm = pyosm.OSMXMLFile('INSERT THE PAHT OF YOUR OSM FILE HERE, e.g. /home/peter/osmpal/new-york.osm')
+osm = pyosm.OSMXMLFile('ENTER THE LOCATION OF THE OPENSTREETMAP .OSM FILE HERE, e.g. /home/peter/osmpal/new-york-small.osm')
+import re
 
 def dist(x1,y1,x2,y2):
     return ((x2-x1)**2+(y2-y1)**2)**0.5
 
+def nearest_node_tagged(nodeID, qry):
+    try:
+        for Nid in osm.nodes:
+            if osm.nodes[Nid]['id'] == nodeID:
+                X1 = osm.nodes[Nid]['lon']
+                Y1 = osm.nodes[Nid]['lat']
+                originalNode = Nid
+        #arbitrarily large starting distance for comparison
+        nearestDist = 100
+        for Nid in osm.nodes:
+            match = re.search('\''+qry+'(.*)', str(osm.nodes[Nid]['tags']))
+            newDist = dist(X1, Y1, osm.nodes[Nid]['lon'], osm.nodes[Nid]['lat'])
+            if match and newDist < nearestDist and Nid is not originalNode:
+                nearestDist = newDist
+                nearestNode = Nid
+                nearestTag = match.group(1)
+        print 'Nearest Distance (not to scale) is: ', nearestDist
+        print '\n Nearest Node for your query is node number: ', nearestNode
+        print '\n Nearest tagged node is: ', nearestTag
+    except:
+        e = sys.exc_info()[0]
+        print 'The following error ocurred: ', e
+
+def farthest_node_tagged(nodeID, qry):
+    try:
+        for Nid in osm.nodes:
+            if osm.nodes[Nid]['id'] == nodeID:
+                X1 = osm.nodes[Nid]['lon']
+                Y1 = osm.nodes[Nid]['lat']
+                originalNode = Nid
+        #arbitrarily small starting distance for comparison
+        farthestDist = 0
+        for Nid in osm.nodes:
+            match = re.search('\''+qry+'(.*)', str(osm.nodes[Nid]['tags']))
+            newDist = dist(X1, Y1, osm.nodes[Nid]['lon'], osm.nodes[Nid]['lat'])
+            if match and newDist > farthestDist and Nid is not originalNode:
+                farthestDist = newDist
+                farthestNode = Nid
+                farthestTag = match.group(1)
+                XFAR = osm.nodes[farthestNode]['lon']
+                YFAR = osm.nodes[farthestNode]['lat']
+        print '\n Farthest Node for your query is node number: ', farthestNode
+        print '\n Farthest tagged node is: ', farthestTag
+    except:
+        e = sys.exc_info()[0]
+        print 'The following error ocurred: ', e
 
 def farthest_node(nodeID):
-    #Need to loop through to get coordinates
+    #First loop through to get coordinates
     for nid in osm.nodes:
         if osm.nodes[nid]['id'] == nodeID:
             X1 = osm.nodes[nid]['lon']
             Y1 = osm.nodes[nid]['lat']
             originalNode = nid
-            print 'X1:', X1
-            print '\n Y1: ', Y1
             print '\n Your original node number is: ', originalNode
     #Arbitrarily small number that variable newDist will be bigger than in comparison below
-    farthestDist = 0.000000000000000000000001
+    farthestDist = 0
+    #Second loop through to calculate distance
     try:
         for Nid in osm.nodes:
             newDist = dist(X1, Y1, osm.nodes[Nid]['lon'], osm.nodes[Nid]['lat'])
@@ -30,8 +75,8 @@ def farthest_node(nodeID):
                 farthestDist = newDist
                 farthestNode = Nid
         if originalNode is not None:
-            print '\n Farthest Node is no: ', farthestNode
-            print '\n Farthest Distance calculated from your original node is : ', farthestDist
+            print '\n Farthest Node number is: ', farthestNode
+            print '\n Farthest Distance calculated (not to scale) from your original node is : ', farthestDist
             XFAR = osm.nodes[farthestNode]['lon']
             YFAR = osm.nodes[farthestNode]['lat']
     except:
@@ -45,11 +90,9 @@ def nearest_node(nodeID):
             X1 = osm.nodes[nid]['lon']
             Y1 = osm.nodes[nid]['lat']
             originalNode = nid
-            print 'X1:', X1
-            print '\n Y1: ', Y1
             print '\n Your original node (starting node) number is :', originalNode
     # Start with arbitrarily large nearestDist and get smaller and smaller distance
-    nearestDist = 100
+    nearestDist = 10000
     try:
         for Nid in osm.nodes:
             newDist = dist(X1, Y1, osm.nodes[Nid]['lon'], osm.nodes[Nid]['lat'])
@@ -57,11 +100,22 @@ def nearest_node(nodeID):
                 nearestDist = newDist
                 nearestNode = Nid
         if originalNode is not None:
-            print '\n Nearest Node is no: ', nearestNode
-            print '\n Nearest Distance calculated: ', nearestDist
+            print '\n Nearest Node number is: ', nearestNode
+            print '\n Nearest Distance calculated (not to scale): ', nearestDist
             XNEAR = osm.nodes[nearestNode]['lon']
             YNEAR = osm.nodes[nearestNode]['lat']
     except:
         e = sys.exc_info()[0]
         print 'The following error ocurred: ', e
 
+#Allow user to choose the function in command line
+# e.g. python osmpalnodesbasic.py nearest_node_tagged 26301254 $'amenity\': u\'cafe'
+if __name__ == "__main__":
+    if sys.argv[1] == 'nearest_node_tagged':
+        nearest_node_tagged(int(sys.argv[2]), sys.argv[3])
+    elif sys.argv[1] == 'farthest_node_tagged':
+        farthest_node_tagged(int(sys.argv[2]), sys.argv[3])
+    elif sys.argv[1] == 'nearest_node':
+        nearest_node(int(sys.argv[2]))
+    elif sys.argv[1] == 'farthest_node':
+        farthest_node(int(sys.argv[2]))
